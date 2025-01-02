4ea191c655139dd6ffe676944cc66d321b2439ad
Sasha Waszaj <swaszajf@gmail.com>
2025-01-02 16:24:49 -0300

    se modifico el index

--- client/RPM/src/components/Home.jsx
+++ client/RPM/src/components/Home.jsx
@@ -6,7 +6,6 @@ import banner2 from "../img/Banner-2.png";
 import banner3 from "../img/Banner-3.png";
 import BrandsCarrousel from "./BrandsCarrousel";
 import NovedadesCarrousel from "./NovedadesCarrousel";
-import { Helmet } from "react-helmet";
 
 const images = [banner1, banner2, banner3];
 
@@ -37,13 +36,6 @@ const Home = () => {
 
   return (
     <div>
-       <>
-      <Helmet>
-        <title>RPM Motos - Inicio</title>
-        <meta name="description" content="Bienvenido a RPM Motos, el mejor lugar para repuestos y accesorios." />
-      </Helmet>
-      <h1>Bienvenido a RPM Motos</h1>
-    </>
       <div className="carousel-container">
         <img
           src={images[currentIndex]}
