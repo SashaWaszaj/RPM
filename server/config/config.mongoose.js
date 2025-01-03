038a4e05cfc016dff0195f4825b83c94965c67a7
Sasha Waszaj <swaszajf@gmail.com>
2025-01-03 10:21:19 -0300

    se modifico el archivo config.mongoose.js para que se enlace a atlas

--- server/config/config.mongoose.js
+++ server/config/config.mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-
-mongoose.connect('mongodb://localhost:27017/RPM_db', {
+// Conexión a MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then(() => console.log('Conectado a MongoDB'))
-    .catch(err => console.error('Error al conectar a MongoDB:', err));
+  .then(() => console.log('Conexión a MongoDB exitosa'))
+  .catch((error) => console.error('Error al conectar a MongoDB:', error));
+
